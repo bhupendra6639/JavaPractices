b2e62fe06bcccdbb4361f9600d66bb802759c677
bhupendra6639 <bhupendrpatil6639@gmail.com>
2025-04-02 00:30:52 +0530

    a

---
+++ 3rd_Assignment/armstrong.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+class Amstrong {
+
+    public static void main(String args[]) {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("enter any number");
+        int number = s1.nextInt();
+        int rem, sum = 0;
+        int temp = number;
+        while (number > 0) {
+            rem = number % 10;
+            sum = sum + (rem * rem * rem);
+            number = number / 10;
+        }
+        if (sum == temp) {
+            System.out.println(temp + ":- number is armstrong");
+        } else {
+            System.out.println(temp + ":- number  is not armstrong");
+
+        }
+
+    }
+}
---
+++ 3rd_Assignment/pallindron_Series.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+class Pallindrome {
+
+    public static void main(String args[]) {
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("enter number for series :-");
+        int number = s1.nextInt();
+        System.out.println("Pallindrome number series are :-");
+        for (int i = 11; i <= number; i++) {
+            int rem, rev = 0;
+            int num = i;
+            int temp = num;
+            while (num > 0) {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+                if (rev == temp) {
+                    System.out.println(temp);
+                }
+            }
+        }
+
+    }
+}
